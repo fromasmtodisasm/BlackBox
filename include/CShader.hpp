f70f2b3fa415f2edb7c733db96d01a0a93badfa8
dk <chiappa24@gmail.com>
2019-03-02 23:57:22 +0300

    Added shader program class blank

--- include/CShader.hpp
+++ include/CShader.hpp
@@ -18,4 +18,24 @@ public:
   bool create();
   bool compile();
   bool bind();
+  GLuint get();
+};
+
+class CShaderProgram {
+private:
+  CShader *m_vertex;
+  CShader *m_fragment;
+  GLuint m_program;
+  GLuint m_status;
+  GLchar infoLog[512];
+
+  bool status();
+public:
+  CShaderProgram();
+  ~CShaderProgram();
+
+  bool create();
+  bool attach(CShader &shader);
+  bool link();
+  bool status(CShader &shader);
 };
