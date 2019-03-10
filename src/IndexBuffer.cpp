ad2eb31e892884a7b9875a9d93b845421bf581fe
fromasmtodisasm <chiappa24@gmail.com>
2019-03-10 16:07:59 +0300

    Added rotation of triangle for testing purpose

--- src/IndexBuffer.cpp
+++ src/IndexBuffer.cpp
@@ -1,23 +1,49 @@
 #include "IndexBuffer.hpp"
+#include <iostream>
 
-IndexBuffer::IndexBuffer(const void *data, size_t count)
+using namespace std;
+
+IndexBuffer::IndexBuffer(const void *data, size_t size) :
+  m_Data(data), m_Size(size)
 {
-  glGenBuffers(1, &m_RendererID);  
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(GLuint), data, GL_STATIC_DRAW);
+  glGenVertexArrays(1, &VAO);
+
+  glBindVertexArray(VAO); 
+  glGenBuffers(1, &VBO);  
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+  // 3. Устанавливаем указатели на вершинные атрибуты
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glIndexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+  glBindVertexArray(0);
 }
 
 IndexBuffer::~IndexBuffer()
 {
-  glDeleteBuffers(1, &m_RendererID);
+  glDeleteBuffers(1, &VBO);
 }
 
 void IndexBuffer::bind()
 {
-  glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO);
 }
 
-void IndexBuffer::unbinnd()
+void IndexBuffer::unbind()
 {
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void IndexBuffer::draw()
+{
+
+  glBindVertexArray(VAO); 
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_Size, m_Data, GL_STATIC_DRAW);
+  // 1. Затем установим указатели на вершинные атрибуты
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+  // 2. Используем нашу шейдерную программу
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glBindVertexArray(0);
 }
--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -1,22 +1,33 @@
 #include "Triangle.hpp"
 #include "VertexBuffer.hpp"
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
 
 using namespace std;
 
 Triangle::Triangle(CShaderProgram *program) :
   m_Shader(program)
 {
-   m_Vertices = {
-      -0.5f, -0.5f, 0.0f,
-       0.5f, -0.5f, 0.0f,
-       0.0f,  0.5f, 0.0f
-    };  
+  m_Vertices = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f,  0.5f, 0.0f
+  };  
+
+  glm::mat4 rot = glm::scale(glm::vec3(0.5, 0.5, 1));
+
+  for (int i = 0; i < m_Vertices.size(); i+=3)
+  {
+    glm::vec4 v = glm::vec4(m_Vertices[i], m_Vertices[i + 1], m_Vertices[i + 2], 0.0);
+    v = v * rot;
+    m_Vertices[i] = v.x;
+    m_Vertices[i + 1] = v.y;
+    m_Vertices[i + 2] = v.z;
+  }
 
   VertexBuffer *vb = new VertexBuffer(m_Vertices.data(),m_Vertices.size()*sizeof(GLfloat));
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-glEnableVertexAttribArray(0);
-
   m_Geom = new Geometry(vb, nullptr);
 }
 
