98f87faa3ba7d4167036240c9d6e22430c785ad1
fromasmtodisasm <chiappa24@gmail.com>
2019-03-11 09:19:30 +0300

    Fixed compile error

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -7,8 +7,9 @@
 using namespace std;
 
 Triangle::Triangle(CShaderProgram *program,float rotAngle) :
-  m_Shader(program), ty(OBJType::TPRIMITIVE)
+  m_Shader(program)
 {
+  ty = OBJType::TPRIMITIVE;
   m_Vertices = {
     -0.5f, -0.5f, 0.0f,
     0.5f, -0.5f, 0.0f,
@@ -17,17 +18,7 @@ Triangle::Triangle(CShaderProgram *program,float rotAngle) :
 
   // glm::mat4 rot = glm::rotate(glm::mat4(1.0f),rotAngle,glm::vec3(1.0, 0.0, 0.0)); 
   glm::mat4 rot = glm::scale(glm::vec3(1.0f, 2.0f, 1.0f));
-  // glm::mat4 rot = glm::rotate(glm::mat4())
 
-  /*for (int i = 0; i < m_Vertices.size(); i+=3)
-  {
-    glm::vec4 v = glm::vec4(m_Vertices[i], m_Vertices[i + 1], m_Vertices[i + 2], 0.0);
-    v = v * rot;
-    m_Vertices[i] = v.x;
-    m_Vertices[i + 1] = v.y;
-    m_Vertices[i + 2] = v.z;
-  }
-  */
   m_Vertices = m_Vertices * rot;
   VertexBuffer *vb = new VertexBuffer(m_Vertices.data(),m_Vertices.size()*sizeof(GLfloat));
   m_Geom = new Geometry(vb, nullptr);
