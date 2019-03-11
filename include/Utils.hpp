bbbeaa99aaa4e912ddc5d7a993eace06597f5c34
fromasmtodisasm <chiappa24@gmail.com>
2019-03-11 09:20:03 +0300

    Added specific win32 system code for get command line args

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,7 +1,15 @@
 #pragma once
 #ifdef _WIN32
+  
+  #define WIN32_LEAN_AND_MEAN
+  #include <windows.h>
   #include <direct.h>
   #define chdir _chdir
+  PCHAR*
+  CommandLineToArgvA(
+      PCHAR CmdLine,
+      int* _argc
+      ); 
 #else
   #include <unistd.h>
 #endif
--- tests/Main.cpp
+++ tests/Main.cpp
@@ -12,7 +12,21 @@
 
 using namespace std;
 
+#ifdef WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+int WINAPI WinMain(
+      HINSTANCE hInstance,
+      HINSTANCE hPrevInstance,
+      PSTR szCmdLine,
+      int iCmdShow) 
+{
+  int argc;
+  char **argv;
+  argv = CommandLineToArgvA(szCmdLine, &argc);
+#else
 int main(int argc, char *argv[]) {
+#endif
   string path;
   bool debug = false;
   if (argc > 1) {
@@ -20,8 +34,7 @@ int main(int argc, char *argv[]) {
       debug = true;
   }
 
-  // chdir((path = getBasePath(string(argv[0]))).c_str());
-  cout << argv[0] << endl;
+  chdir((path = getBasePath(string(argv[0]))).c_str());
   cout << path << endl;
 
   IGame *game = CreateIGame("MyGame");
