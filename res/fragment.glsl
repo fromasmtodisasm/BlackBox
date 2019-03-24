6eb5137de05c1cd231615a23a085665d6991db3f
hackman <chiappa24@gmail.com>
2019-03-25 00:23:14 +0300

    Updated shaders

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -1,6 +1,7 @@
 #version 330 core
 out vec4 color;
+in vec3 pos;
 void main()
 {
-    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);;
+    color = vec4(pos, 1.0f);;
 } 
--- res/vertex.glsl
+++ res/vertex.glsl
@@ -1,8 +1,13 @@
 #version 330 core
 
 layout (location = 0) in vec3 position;
+uniform float offset;
+uniform mat4x4 rotate;
+
+out vec3 pos;
 
 void main()
 {
-    gl_Position = vec4(position.x, position.y, position.z, 1.0);
+    gl_Position = rotate * vec4(position.x, position.y, position.z, 1.0);
+    pos = gl_Position.xyz;
 }
