763ec7504129f532641fd33661a1a535f4bd2c68
hackman <chiappa24@gmail.com>
2019-05-17 15:18:03 +0300

    Removed winmain, todo: make it in separate platform specific file

--- tests/Main.cpp
+++ tests/Main.cpp
@@ -14,21 +14,7 @@
 
 using namespace std;
 
-#if 0//#ifdef _MSC_VER
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-int WINAPI WinMain(
-      HINSTANCE hInstance,
-      HINSTANCE hPrevInstance,
-      PSTR szCmdLine,
-      int iCmdShow) 
-{
-  int argc;
-  char **argv;
-  argv = CommandLineToArgvA(szCmdLine, &argc);
-#else
 int main(int argc, char *argv[]) {
-#endif
   string path;
   bool debug = false;
   if (argc > 1) {
