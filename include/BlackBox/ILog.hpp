270958fef52167971055ca57048d7349ef788c82
chiappa <chiappa@raa-st.com>
2019-08-14 10:33:01 +0300

    Добавлен недологгер

--- include/BlackBox/ILog.hpp
+++ include/BlackBox/ILog.hpp
@@ -5,4 +5,5 @@ struct ILog
   virtual void Clear() = 0;
   virtual void AddLog(const char* fmt, ...) = 0;
   virtual void Draw(const char* title, bool* p_open) = 0;
+  virtual void Release() = 0;
 };
