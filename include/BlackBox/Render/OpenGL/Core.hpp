6cb03d227743d8886dfe23899d7d7c4840a834cb
denis <chiappa24@gmail.com>
2019-11-01 09:30:07 +0300

    remove unicode invisiable symbols

--- include/BlackBox/Render/OpenGL/Core.hpp
+++ include/BlackBox/Render/OpenGL/Core.hpp
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #ifdef GLAD_LOADER
 //#define _WINDOWS_
 #pragma warning(push)
@@ -62,8 +62,8 @@ public:
 		glPopDebugGroup();
 	}
 private:
-	static void APIENTRY callBack(GLenum source​, GLenum type​, GLuint id​,
-	GLenum severity​, GLsizei length​, const GLchar* message​, const void* userParam​);
+	static void APIENTRY callBack(GLenum source, GLenum type, GLuint id,
+	GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
 
 	static inline const char* SOURCE_TO_STRING(GLenum s)
 	{
