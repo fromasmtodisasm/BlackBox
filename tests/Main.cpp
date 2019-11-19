197c6ce6b3ad1e28ffa5e5e6770c917896b9215d
chiappa <chiappa@raa-st.com>
2019-11-19 19:27:04 +0300

    code cleanup

--- tests/Main.cpp
+++ tests/Main.cpp
@@ -13,15 +13,13 @@
 
 /*##############################################*/
 
-
 /*----------------------------------------------*/
-
 
 /*##############################################*/
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   string path;
   bool debug = false;
   if (argc > 1) {
@@ -30,39 +28,39 @@ int main(int argc, char *argv[]) {
   }
 
 #if NEED_FILE_SYSTEM
-	std::filesystem::current_path("../../");
-	cout << "current path: " << std::filesystem::current_path() << endl;
+  std::filesystem::current_path("../../");
+  cout << "current path: " << std::filesystem::current_path() << endl;
 #endif
 
   std::string cmdline;
 
-	if (argc > 1)
-	{
-		cmdline += argv[1];
-		for (int i = 2; i < argc; i++)
-		{
-			cmdline = cmdline + " " + argv[i];
-		}
-	}
+  if (argc > 1)
+  {
+    cmdline += argv[1];
+    for (int i = 2; i < argc; i++)
+    {
+      cmdline = cmdline + " " + argv[i];
+    }
+  }
 
-	SSystemInitParams params;
+  SSystemInitParams params;
 
-	time_t t = time(nullptr);
-	std::stringstream ss;
-	ss << "logs/" << std::put_time(std::localtime(&t), "%H-%M-%S") << ".txt";
-	params.sLogFileName = _strdup(ss.str().c_str());
+  time_t t = time(nullptr);
+  std::stringstream ss;
+  ss << "logs/" << std::put_time(std::localtime(&t), "%H-%M-%S") << ".txt";
+  params.sLogFileName = _strdup(ss.str().c_str());
 
-	snprintf(params.szSystemCmdLine, 512, "%s", cmdline.c_str());
-  ISystem*pSystem = CreateSystemInterface(params);
+  snprintf(params.szSystemCmdLine, 512, "%s", cmdline.c_str());
+  ISystem* pSystem = CreateSystemInterface(params);
   if (!pSystem->Init())
   {
     pSystem->Release();
     return EXIT_FAILURE;
   }
-	pSystem->GetILog()->Log("[OK] ISystem created\n");
-	pSystem->GetILog()->Log("[INFO] Current working directory: %s\n", path.c_str());
+  pSystem->GetILog()->Log("[OK] ISystem created\n");
+  pSystem->GetILog()->Log("[INFO] Current working directory: %s\n", path.c_str());
   pSystem->Start();
-	pSystem->Release();
+  pSystem->Release();
 
   return 0;
 }
