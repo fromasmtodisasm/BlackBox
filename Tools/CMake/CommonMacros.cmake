ba81cad736dc7edef622f52b849dbbb1027dc86b
HackMan <chiappa24@gmail.com>
2020-01-22 19:15:14 +0300

    - update project

--- Tools/CMake/CommonMacros.cmake
+++ Tools/CMake/CommonMacros.cmake
@@ -44,4 +44,11 @@ macro(add_package Package)
 	endif()
 endmacro()
 
+macro(add_subsystem subsystem)
+	add_subdirectory(${ENGINE_DIR}/${subsystem} ${subsystem})
+	target_link_libraries(${BLACKBOX_PROJECT} PRIVATE ${subsystem})
+	set_target_properties(${subsystem} PROPERTIES FOLDER "Engine")
+
+endmacro()
+
 CommonMacrosInit()
