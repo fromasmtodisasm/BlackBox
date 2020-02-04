a322ced048bf25ba3e3d7bd2d43e0b86b1cda096
fromasmtodisasm <you@example.com>
2020-02-04 04:07:33 +0300

    refact

--- src/Launcher/LinuxLauncher/Main.cpp
+++ src/Launcher/LinuxLauncher/Main.cpp
@@ -8,21 +8,12 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+  int status = EXIT_FAILURE;
   string path;
-  bool debug = false;
-  if (argc > 1) {
-    if (string(argv[1]) == "-debug")
-      debug = true;
-  }
-
   std::string cmdline;
-  if (argc > 1)
+  for (int i = 0; i < argc; i++)
   {
-    cmdline += argv[1];
-    for (int i = 2; i < argc; i++)
-    {
-      cmdline = cmdline + " " + argv[i];
-    }
+    cmdline = cmdline + " " + argv[i];
   }
 
   SSystemInitParams params;
@@ -31,19 +22,17 @@ int main(int argc, char* argv[]) {
   std::stringstream ss;
   ss << "logs/" << std::put_time(std::localtime(&t), "%H-%M-%S") << ".txt";
   params.sLogFileName = strdup(ss.str().c_str());
-  //params.bDedicatedServer = true;
 
   snprintf(params.szSystemCmdLine, 512, "%s", cmdline.c_str());
   ISystem* pSystem = CreateSystemInterface(params);
-  if (!pSystem->Init())
+  if (pSystem->Init())
   {
-    pSystem->Release();
-    return EXIT_FAILURE;
+    pSystem->GetILog()->Log("[OK] ISystem created\n");
+    pSystem->GetILog()->Log("[INFO] Current working directory: %s\n", path.c_str());
+    pSystem->Start();
+    status = EXIT_SUCCESS;
   }
-  pSystem->GetILog()->Log("[OK] ISystem created\n");
-  pSystem->GetILog()->Log("[INFO] Current working directory: %s\n", path.c_str());
-  pSystem->Start();
   pSystem->Release();
 
-  return 0;
+  return status;
 }
