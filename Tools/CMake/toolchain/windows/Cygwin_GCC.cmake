e7e590aa7a2f3c76b975da13cc848076bc077047
anon <anonymous@anon.com>
2020-02-21 15:05:50 +0300

    -

---
+++ Tools/CMake/toolchain/windows/Cygwin_GCC.cmake
@@ -0,0 +1,27 @@
+set(CMAKE_CONFIGURATION_TYPES Debug Profile Release)
+set(CMAKE_CONFIGURATION_TYPES 
+  "${CMAKE_CONFIGURATION_TYPES}" CACHE STRING "Reset the configurations to what we need" FORCE
+)
+
+set(BUILD_CPU_ARCHITECTURE x64)
+set(BUILD_PLATFORM Cygwin64)
+set(LINUX 1)
+set(LINUX64 1)
+
+
+set(OUTPUT_DIRECTORY_NAME "cygwin_x64_gcc")
+
+#[[
+set(CMAKE_C_COMPILER gcc-4.9)
+set(CMAKE_CXX_COMPILER g++-4.9)
+]]
+
+add_definitions(-DLINUX64)
+
+message(STATUS "BUILD_CPU_ARCHITECTURE = ${BUILD_CPU_ARCHITECTURE}")
+
+#include ("${CMAKE_CURRENT_LIST_DIR}/../../CRYENGINE-GCC.cmake")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DLINUX -D__linux__ -DOPENGL" CACHE STRING "C Common Flags" FORCE)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DLINUX -D__linux__ -DOPENGL" CACHE STRING "C++ Common Flags" FORCE)
+
