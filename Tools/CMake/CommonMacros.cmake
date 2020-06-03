37ab773239f8491dbefe7b87762462e4d10ba4f6
name <email>
2020-06-03 20:15:55 +0300

    -

--- Tools/CMake/CommonMacros.cmake
+++ Tools/CMake/CommonMacros.cmake
@@ -91,7 +91,7 @@ endfunction()
 
 function(add_package Package)
 	find_package(${ARGV})
-  if (UNIX_OR_CYGWIN)
+  if (UNIX_OR_CYGWIN OR (NOT DEFINED VCPKG_INSTALLER))
     add_package_unix_or_cygwin(${Package})
   else()
     add_package_msvc(${Package})
