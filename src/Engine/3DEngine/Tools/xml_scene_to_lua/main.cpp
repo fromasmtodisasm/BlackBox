675baa0d6960c1410b6abfa3af168607484e3c02
denis koveshnikov <chiappa@raa-st.com>
2020-07-13 17:26:02 +0300

    -

---
+++ src/Engine/3DEngine/Tools/xml_scene_to_lua/main.cpp
@@ -0,0 +1,227 @@
+#include <tinyxml2.h> 
+#include <vector>
+#include <string>
+#include <memory>
+#include <BlackBox/Core/MathHelper.hpp>
+
+using namespace tinyxml2;
+#define XMLCheckResult(a_eResult) if (a_eResult != XML_SUCCESS) { printf("Error: %i\n", a_eResult); return false; }
+
+struct Mesh
+{
+	std::string m_Path;
+};
+
+struct Transform
+{
+	Vec3 Position;	
+	Vec3 Rotation;	
+	Vec3 Scale;	
+};
+
+struct Object
+{
+	std::string Name;
+	std::string Type;
+	std::string Material;
+	bool Visible;
+	bool Transparent;
+	Mesh Geometry;
+	Transform Transform;
+};
+
+struct Camera
+{
+	std::string Name;
+	Transform Transform;
+};
+
+struct Scene
+{
+	std::vector<std::shared_ptr<Object>> Objects;
+};
+
+class Serializator
+{
+	public:
+	Serializator(Scene& scene)
+		  : m_Scene(scene) {}
+	bool Save(const char* as = "")
+	{
+		return false;	
+	}
+	bool Load(const char* name)
+	{
+			
+		tinyxml2::XMLDocument xmlDoc;
+		XMLError eResult = xmlDoc.LoadFile(name);
+		XMLCheckResult(eResult);
+
+		XMLNode* pScene = xmlDoc.FirstChild();
+		if (pScene == nullptr) return false;
+
+		XMLElement* objects = pScene->FirstChildElement("object");
+
+		if (objects == nullptr) return false;
+		while (objects != nullptr)
+		{
+			LoadObject(objects);
+			objects = objects->NextSiblingElement("object");
+		}
+
+		XMLElement* terrain = pScene->FirstChildElement("terrain");
+		if (terrain != nullptr)
+		{
+			LoadTerrain(terrain);
+		}
+
+		XMLElement* lights = pScene->FirstChildElement("light");
+		while (lights != nullptr)
+		{
+			lights = lights->NextSiblingElement("light");
+		}
+
+
+		#ifdef SAVE_LOAD_TAGPOINTS
+		XMLElement* tag_points = pScene->FirstChildElement("tag_points");
+		if (tag_points == nullptr) return false;
+		while (tag_points != nullptr)
+		{
+		loadTagPoint(tag_points);
+		tag_points = tag_points->NextSiblingElement("tagpoint");
+		}
+		#endif // SAVE_LOAD_TAGPOINTS
+
+		XMLElement* cameras = pScene->FirstChildElement("camera");
+		if (cameras == nullptr) return false;
+		while (cameras != nullptr)
+		{
+			LoadCamera(cameras);
+			cameras = cameras->NextSiblingElement("camera");
+		}
+
+	}
+	bool LoadDefault()
+	{
+	
+	}
+
+	void LoadTerrain(tinyxml2::XMLElement* terrain)
+	{
+	
+	}
+	void LoadObject(tinyxml2::XMLElement* object)
+	{
+		std::shared_ptr<Object> obj = std::make_shared<Object>();
+		std::string material("default");
+		const char* objectName = nullptr;
+		const char* objectType = nullptr;
+		bool objectTransparent = false;
+		bool objectVisible = false;
+		const char* meshPath = nullptr;
+		const char* materialName = nullptr;
+		XMLElement* mesh = nullptr;
+		XMLElement* materialElement = nullptr;
+		Transform transform;
+
+		objectName = object->Attribute("name");
+		if (objectName == nullptr)
+		return;
+		objectType = object->Attribute("type");
+		if (objectType == nullptr)
+		objectType = "object";
+		{
+		if (object->BoolAttribute("transparent"))
+		  objectTransparent = true;
+		if (object->BoolAttribute("visible"))
+		  objectVisible = true;
+		mesh = object->FirstChildElement("mesh");
+		if (mesh == nullptr)
+		  return;
+		meshPath = mesh->Attribute("name");
+		if (meshPath == nullptr)
+		  return;
+		//obj = ObjectManager::instance()->getObject(meshPath, objectType, callback);
+		obj->Name;
+		}
+		if (obj == nullptr)
+		return;
+		materialElement = object->FirstChildElement("material");
+		materialName = materialElement->Attribute("name");
+		if (materialName != nullptr)
+		  material = materialName;
+
+		auto uv = object->FirstChildElement("uv");
+		if (uv != nullptr)
+		{
+			auto scaleX = uv->FloatAttribute("scaleX", 1.0);
+			auto scaleY = uv->FloatAttribute("scaleY", 1.0);
+			//obj->uvMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(scaleX, scaleY, 1.0f));
+		}
+		else
+		{
+			//obj->uvMatrix = glm::mat4(1.0f);
+		}
+
+		transform = loadTransform(*object);
+		obj->Transform = transform;
+		obj->Transparent = objectTransparent;
+		obj->Visible = objectVisible;
+		obj->Material = material;
+		m_Scene.Objects.push_back(obj);
+	
+	}
+	void LoadMesh(tinyxml2::XMLElement* mesh)
+	{
+	
+	}
+	void LoadLight(tinyxml2::XMLElement* light)
+	{
+	
+	}
+	Transform loadTransform(tinyxml2::XMLElement& object)
+	{
+		return Transform();
+	}
+	void LoadCamera(tinyxml2::XMLElement* element)
+	{
+	
+	}
+	void LoadTagPoint(tinyxml2::XMLElement* element)
+	{
+
+	}
+
+	void SaveLights(tinyxml2::XMLDocument& xmlDoc, tinyxml2::XMLNode* pScene)
+	{
+	
+	}
+	void SaveObject(tinyxml2::XMLDocument& xmlDoc, Object& obj, tinyxml2::XMLNode* pScene)
+	{
+	
+	}
+	tinyxml2::XMLElement* SaveTransform(tinyxml2::XMLDocument& xmlDoc, Transform* transform)
+	{
+	
+	}
+	//tinyxml2::XMLElement* saveLight(tinyxml2::XMLDocument& xmlDoc, BaseLight* light);
+	tinyxml2::XMLElement* SaveCamera(tinyxml2::XMLDocument& xmlDoc, const Camera& camera)
+	{
+	
+	}
+	private:
+	Scene& m_Scene;
+};
+
+int main(int argc, char* argv[])
+{
+	argc = 2;
+	if (argc < 2)
+		return -1;
+	std::string scene_name = "./res/scenes/test.xml";
+	//scene_name = argv[1]
+	Scene scene;
+	Serializator serializtor(scene);
+	serializtor.Load(scene_name.c_str());
+	serializtor.Save();
+}
