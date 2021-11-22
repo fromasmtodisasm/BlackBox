58c4caa39c891b029bd3dcc5cf7ee99bf29ccb1d
chiappa <chiappa@raa-st.com>
2021-11-22 12:16:42 +0300

    - updated imgui

--- Code/Engine/GUI/load_imgui.sh
+++ Code/Engine/GUI/load_imgui.sh
@@ -9,3 +9,9 @@ curl -L https://raw.githubusercontent.com/ocornut/imgui/docking/imstb_rectpack.h
 curl -L https://raw.githubusercontent.com/ocornut/imgui/docking/imstb_textedit.h --output imstb_textedit.h
 curl -L https://raw.githubusercontent.com/ocornut/imgui/docking/imstb_truetype.h --output imstb_truetype.h
 
+curl -L https://raw.githubusercontent.com/ocornut/imgui/docking/backends/imgui_impl_dx11.h --output ImGuiBackend/Vanila/imgui_impl_dx11.h
+curl -L https://raw.githubusercontent.com/ocornut/imgui/docking/backends/imgui_impl_dx11.cpp --output ImGuiBackend/Vanila/imgui_impl_dx11.cpp
+
+curl -L https://raw.githubusercontent.com/ocornut/imgui/docking/backends/imgui_impl_sdl.cpp --output ImGuiBackend/Vanila/imgui_impl_sdl.cpp
+curl -L https://raw.githubusercontent.com/ocornut/imgui/docking/backends/imgui_impl_sdl.h --output ImGuiBackend/Vanila/imgui_impl_sdl.h
+
